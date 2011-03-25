625c0cc2cf642fd5ae8f3aa9049b5e5b35b4c11b
Sava Marinkov <s.marinkov@tue.nl>
2011-03-25 16:25:58 +0000

    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/dev/tue_actuation_dev/amigo_battery_manager_dev@2878 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ src/test_client.cpp
@@ -0,0 +1,26 @@
+#include <ros/ros.h>
+#include <soem_beckhoff_drivers/AnalogMsg.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "battery_test_client");
+  ros::NodeHandle nh;
+  
+  ros::Publisher target_pub = nh.advertise<soem_beckhoff_drivers::AnalogMsg>("battery", 50);
+  
+  soem_beckhoff_drivers::AnalogMsg msg;
+  
+  msg.values.resize(2);
+  
+	msg.values[0] = 6;
+	msg.values[1] = 0.0;
+
+  ros::Rate rate(1.0);
+  
+  while (ros::ok()){
+	ROS_INFO("Publish");  
+    target_pub.publish(msg);
+    rate.sleep();
+  }
+  
+  return true;
+}
