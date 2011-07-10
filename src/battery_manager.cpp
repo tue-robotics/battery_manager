8cf717df9604100c1798ee7494b06772de732469
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2011-07-10 11:35:22 +0000

    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_perception/amigo_battery_manager@5217 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- src/battery_manager.cpp
+++ src/battery_manager.cpp
@@ -17,9 +17,12 @@ bool alarm_state = false;
 double low_power_margin_ch1, high_power_margin_ch1, low_power_margin_ch2, high_power_margin_ch2, nominal_power, conversion_factor, alarm_period;
 std_msgs::String alarm_msg;
 
+bool fuse1, fuse2, fuse3, fuse4;
+double power;
+
 void digitalCallback(const soem_beckhoff_drivers::DigitalMsg::ConstPtr& msg)
 {
-	vector<diagnostic_msgs::DiagnosticStatus> statuses;
+	/*vector<diagnostic_msgs::DiagnosticStatus> statuses;
 	diagnostic_updater::DiagnosticStatusWrapper status;
 
 	if (msg->values[0])
@@ -65,65 +68,12 @@ void digitalCallback(const soem_beckhoff_drivers::DigitalMsg::ConstPtr& msg)
 	diag_msg.status = statuses;
 	diag_msg.header.stamp = ros::Time::now();
 
-	diag_pub.publish(diag_msg);
+	diag_pub.publish(diag_msg);*/
 }
 void analogCallback(const soem_beckhoff_drivers::AnalogMsg::ConstPtr& msg)
 {
-	vector<diagnostic_msgs::DiagnosticStatus> statuses;
-	diagnostic_updater::DiagnosticStatusWrapper status;
-
-	status.addf("Battery level", "%f", msg->values[0]*conversion_factor);
-
-	status.name = "Battery";
-
-
-
 	ROS_DEBUG("Battery value: %f", msg->values[0]);
-
-	if (msg->values[0] < low_power_margin_ch1) {
-		alarm_msg.data = string("Battery low");
-		alarm_state = true;
-		status.level = 1;
-		status.message = "Battery low";
-
-	} else if (msg->values[0] > high_power_margin_ch1) {
-		alarm_msg.data = string("Power too high");
-		alarm_state = true;
-		status.level = 1;
-		status.message = "Power too high";
-	} else {
-		alarm_msg.data = "";
-		alarm_state = false;
-		alarm_count = 0;	
-		status.level = 0;
-	}
-
-	if (alarm_state) {
-		if (alarm_count == 0) {
-			time_init = ros::Time::now();
-		}
-		if (alarm_count < max_alarms) {
-			pub.publish(alarm_msg);
-			alarm_count++;
-		}
-	}
-
-	time_current = ros::Time::now();	
-
-	if (alarm_state && (time_current.toSec()-time_init.toSec() > alarm_period)) {
-		alarm_count = 0;
-		alarm_state = false;
-		alarm_msg.data = "";
-	}
-
-
-	statuses.push_back(status);
-
-	diagnostic_msgs::DiagnosticArray diag_msg;
-	diag_msg.status = statuses;
-	diag_msg.header.stamp = ros::Time::now();
-
-	diag_pub.publish(diag_msg);
+	power = msg->values[0];
 }
 
 int main(int argc, char **argv)
@@ -153,6 +103,92 @@ int main(int argc, char **argv)
 	{
 		ros::spinOnce();
 		loop_rate.sleep();
+		vector<diagnostic_msgs::DiagnosticStatus> statuses;
+		diagnostic_updater::DiagnosticStatusWrapper status;
+
+		status.addf("Battery level", "%f", power*conversion_factor);
+
+		status.name = "Battery";
+
+		ROS_DEBUG("Battery value: %f", power);
+
+		alarm_msg.data = "";
+		alarm_state = false;
+		alarm_count = 0;
+		status.level = 0;
+
+		if (power < low_power_margin_ch1) {
+			alarm_msg.data = string("Battery low");
+			alarm_state = true;
+			status.level = 1;
+			status.message = "Battery low";
+		} else if (power > high_power_margin_ch1) {
+			alarm_msg.data = string("Power too high");
+			alarm_state = true;
+			status.level = 1;
+			status.message = "Power too high";
+		}
+		if (fuse1)
+		{
+			status.addf("Fuse RB", "broken");
+			status.level = 2;
+			status.message = "Fuse broken";
+		}
+		else
+			status.addf("Fuse RB", "correct");
+		if (fuse2)
+		{
+			status.addf("Fuse RF", "broken");
+			status.level = 2;
+			status.message = "Fuse broken";
+		}
+		else
+			status.addf("Fuse RF", "correct");
+		if (fuse3)
+		{
+			status.addf("Fuse LF", "broken");
+			status.level = 2;
+			status.message = "Fuse broken";
+		}
+		else
+			status.addf("Fuse LF", "correct");
+		if (fuse4)
+		{
+			status.addf("Fuse LB", "broken");
+			status.level = 2;
+			status.message = "Fuse broken";
+		}
+		else
+			status.addf("Fuse LB", "correct");
+
+
+		if (alarm_state) {
+			if (alarm_count == 0) {
+				time_init = ros::Time::now();
+			}
+			if (alarm_count < max_alarms) {
+				pub.publish(alarm_msg);
+				alarm_count++;
+			}
+		}
+
+		time_current = ros::Time::now();
+
+		if (alarm_state && (time_current.toSec()-time_init.toSec() > alarm_period)) {
+			alarm_count = 0;
+			alarm_state = false;
+			alarm_msg.data = "";
+		}
+
+
+		statuses.push_back(status);
+
+		diagnostic_msgs::DiagnosticArray diag_msg;
+		diag_msg.status = statuses;
+		diag_msg.header.stamp = ros::Time::now();
+
+		diag_pub.publish(diag_msg);
+
 	}
 	return 0;
 }
